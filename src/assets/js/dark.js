02af0646e14c3e96f5687453603b3b2a3551c0b0
Mauritz Marais <mauritz303@gmail.com>
2024-02-27 13:21:32 +0200

    added theme toggle image update on theme change

--- src/assets/js/dark.js
+++ src/assets/js/dark.js
@@ -2,6 +2,12 @@
 let storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
 if (storedTheme) {
     document.documentElement.setAttribute('data-theme', storedTheme);
+
+    // theme image
+    document.querySelector("#theme-toggle img").src = `/assets/svgs/${storedTheme}_mode.svg`;
+
+    // text
+    // document.querySelector("#theme-toggle").textContent = `${storedTheme}`;
 }
 
 // event listener on change
@@ -14,7 +20,12 @@ document.querySelector('#theme-toggle').addEventListener('click', () => {
         targetTheme = "dark";
     }
 
-    // update theme image src*
+    // update theme image
+    document.querySelector("#theme-toggle img").src = `/assets/svgs/${targetTheme}_mode.svg`;
+
+    // update text
+    // document.querySelector("#theme-toggle").textContent = `${targetTheme}`;
+     
 
     document.documentElement.setAttribute('data-theme', targetTheme);
     localStorage.setItem('theme', targetTheme);
